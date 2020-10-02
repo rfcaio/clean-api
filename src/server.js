d5f28728d07acfedb976f6a714c3ad845409910d
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-02 18:39:41 -0300

    add list products endpoint

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 const express = require('express')
 
 const CreateProduct = require('./interactors/CreateProduct')
+const ListProducts = require('./interactors/ListProducts')
 
 const MemoryProductGateway = require('./adapters/MemoryProductGateway')
 
 const memoryProductGateway = new MemoryProductGateway()
 const createProduct = new CreateProduct(memoryProductGateway)
+const listProducts = new ListProducts(memoryProductGateway)
 
 const server = express()
 
 server.use(express.json())
+
+server.get('/product', async (req, res) => {
+  const response = await listProducts.listAll()
+
+  if (response.statusCode === 200) {
+    const { products, statusCode } = response
+    return res.status(statusCode).json({ products })
+  }
+  return res.status(response.statusCode).json({ message: response.message })
+})
 
 server.post('/product', async (req, res) => {
   const { name, price } = req.body
