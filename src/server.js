2b90d401849bf2def192225cbc7d7db2cfd913fa
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-02 17:34:21 -0300

    add create product endpoint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
 const express = require('express')
 
+const CreateProduct = require('./interactors/CreateProduct')
+
+const MemoryProductGateway = require('./adapters/MemoryProductGateway')
+
+const memoryProductGateway = new MemoryProductGateway()
+const createProduct = new CreateProduct(memoryProductGateway)
+
 const server = express()
+
+server.use(express.json())
+
+server.post('/product', async (req, res) => {
+  const { name, price } = req.body
+  const { message, statusCode } = (
+    await createProduct.create({ name, price: parseFloat(price) })
+  )
+  return res.status(statusCode).json({ message })
+})
 
 server.listen(3001)
