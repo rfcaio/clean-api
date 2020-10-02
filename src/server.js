7b64f5733bdd3fdf4f67a91b855c0c15259fcaea
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-02 19:34:39 -0300

    add get product by id endpoint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 const CreateProduct = require('./interactors/CreateProduct')
+const GetProductById = require('./interactors/GetProductById')
 const ListProducts = require('./interactors/ListProducts')
 
 const MemoryProductGateway = require('./adapters/MemoryProductGateway')
 
 const memoryProductGateway = new MemoryProductGateway()
 const createProduct = new CreateProduct(memoryProductGateway)
+const getProductById = new GetProductById(memoryProductGateway)
 const listProducts = new ListProducts(memoryProductGateway)
 
 const server = express()
@@ -19,6 +21,17 @@ server.get('/product', async (req, res) => {
   if (response.statusCode === 200) {
     const { products, statusCode } = response
     return res.status(statusCode).json({ products })
+  }
+  return res.status(response.statusCode).json({ message: response.message })
+})
+
+server.get('/product/:id', async (req, res) => {
+  const { id } = req.params
+  const response = await getProductById.getById({ id: parseInt(id, 10) })
+
+  if (response.statusCode === 200) {
+    const { product, statusCode } = response
+    return res.status(statusCode).json(product)
   }
   return res.status(response.statusCode).json({ message: response.message })
 })
