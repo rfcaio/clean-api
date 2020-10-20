2b5553ce29b93be17c67f562d1a9397523eefe88
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-19 23:46:11 -0300

    refactor create product interactor tests

--- src/interactors/CreateProductInteractor.test.js
+++ src/interactors/CreateProductInteractor.test.js
@@ -9,28 +9,33 @@ const CREATE_PRODUCT_REQUEST = {
 // FIXME: testing product entity real implementation
 describe('CreateProductInteractor', () => {
   describe('exec', () => {
-    test('return an error response in product entity error', async () => {
-      const createProductInteractor = new CreateProductInteractor({
-        create: () => {}
-      })
+    test('return an error response in create product request error', async () => {
+      const productGateway = { create: jest.fn() }
+      const createProductInteractor = new CreateProductInteractor(productGateway)
       const response = await createProductInteractor.exec({ name: '', price: null })
+
+      expect(productGateway.create).not.toHaveBeenCalled()
       expect(response.statusCode).toEqual(400)
     })
 
     test('return an error response in gateway errors', async () => {
-      const createProductInteractor = new CreateProductInteractor({
-        create: jest.fn('productGateway.create').mockRejectedValue(new Error('Product gateway error.'))
-      })
+      const productGateway = {
+        create: jest.fn().mockRejectedValue(new Error('Product gateway error.'))
+      }
+      const createProductInteractor = new CreateProductInteractor(productGateway)
       const response = await createProductInteractor.exec(CREATE_PRODUCT_REQUEST)
+
+      expect(productGateway.create).toHaveBeenCalledWith(CREATE_PRODUCT_REQUEST)
       expect(response.statusCode).toEqual(400)
       expect(response.message).toEqual('Product gateway error.')
     })
 
-    test('return a success response', async () => {
-      const createProductInteractor = new CreateProductInteractor({
-        create: jest.fn('productGateway.create').mockResolvedValue()
-      })
+    test('return a response for product creation', async () => {
+      const productGateway = { create: jest.fn().mockResolvedValue() }
+      const createProductInteractor = new CreateProductInteractor(productGateway)
       const response = await createProductInteractor.exec(CREATE_PRODUCT_REQUEST)
+
+      expect(productGateway.create).toHaveBeenCalledWith(CREATE_PRODUCT_REQUEST)
       expect(response.statusCode).toEqual(201)
       expect(response.message).toEqual('Product successfully created.')
     })
