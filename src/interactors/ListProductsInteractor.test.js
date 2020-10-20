9101746f3f240597085563a57243438249954df0
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-19 23:15:14 -0300

    refactor list products interactor tests

--- src/interactors/ListProductsInteractor.test.js
+++ src/interactors/ListProductsInteractor.test.js
@@ -6,11 +6,12 @@ describe('ListProductsInteractor', () => {
       listAll: jest.fn().mockRejectedValue(new Error('Product gateway error.'))
     })
     const response = await listProductsInteractor.exec()
+
     expect(response.statusCode).toEqual(400)
     expect(response.message).toEqual('Product gateway error.')
   })
 
-  test('return a success response', async () => {
+  test('return all products successfully', async () => {
     const PRODUCT_LIST = [
       { id: '1', name: 'Arroz Alteza 1Kg', price: 6.49 },
       { id: '2', name: 'Feijão Kicaldo 1Kg', price: 7.99 }
@@ -19,6 +20,7 @@ describe('ListProductsInteractor', () => {
       listAll: jest.fn().mockReturnValue(PRODUCT_LIST)
     })
     const response = await listProductsInteractor.exec()
+
     expect(response.statusCode).toEqual(200)
     expect(response.products).toEqual(PRODUCT_LIST)
   })
