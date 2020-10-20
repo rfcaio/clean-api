330cbdb4fe1a6bf8548a45612cc0c84ce8b31f51
Caio Rodrigues <rfcaio90@gmail.com>
2020-10-19 23:51:18 -0300

    add missing describe block

--- src/interactors/GetProductByIdInteractor.test.js
+++ src/interactors/GetProductByIdInteractor.test.js
@@ -1,36 +1,38 @@
 const GetProductByIdInteractor = require('./GetProductByIdInteractor')
 
 describe('GetProductByIdInteractor', () => {
-  test('return an error response in gateway errors', async () => {
-    const productGateway = {
-      getById: jest.fn().mockRejectedValue(new Error('Product gateway error.'))
-    }
-    const getProductByIdInteractor = new GetProductByIdInteractor(productGateway)
-    const response = await getProductByIdInteractor.exec('1')
+  describe('exec', () => {
+    test('return an error response in gateway errors', async () => {
+      const productGateway = {
+        getById: jest.fn().mockRejectedValue(new Error('Product gateway error.'))
+      }
+      const getProductByIdInteractor = new GetProductByIdInteractor(productGateway)
+      const response = await getProductByIdInteractor.exec('1')
 
-    expect(productGateway.getById).toHaveBeenCalledWith('1')
-    expect(response.statusCode).toEqual(400)
-    expect(response.message).toEqual('Product gateway error.')
-  })
+      expect(productGateway.getById).toHaveBeenCalledWith('1')
+      expect(response.statusCode).toEqual(400)
+      expect(response.message).toEqual('Product gateway error.')
+    })
 
-  test('return an error response for product not found', async () => {
-    const productGateway = { getById: jest.fn().mockReturnValue(null) }
-    const getProductByIdInteractor = new GetProductByIdInteractor(productGateway)
-    const response = await getProductByIdInteractor.exec('1')
+    test('return an error response for product not found', async () => {
+      const productGateway = { getById: jest.fn().mockReturnValue(null) }
+      const getProductByIdInteractor = new GetProductByIdInteractor(productGateway)
+      const response = await getProductByIdInteractor.exec('1')
 
-    expect(productGateway.getById).toHaveBeenCalledWith('1')
-    expect(response.statusCode).toEqual(404)
-    expect(response.message).toEqual('Product not found.')
-  })
+      expect(productGateway.getById).toHaveBeenCalledWith('1')
+      expect(response.statusCode).toEqual(404)
+      expect(response.message).toEqual('Product not found.')
+    })
 
-  test('return a product by id successfully', async () => {
-    const product = { id: '1', name: 'Arroz Alteza 1Kg', price: 6.49 }
-    const productGateway = { getById: jest.fn().mockReturnValue(product) }
-    const getProductByIdInteractor = new GetProductByIdInteractor(productGateway)
-    const response = await getProductByIdInteractor.exec('1')
+    test('return a product by id successfully', async () => {
+      const product = { id: '1', name: 'Arroz Alteza 1Kg', price: 6.49 }
+      const productGateway = { getById: jest.fn().mockReturnValue(product) }
+      const getProductByIdInteractor = new GetProductByIdInteractor(productGateway)
+      const response = await getProductByIdInteractor.exec('1')
 
-    expect(productGateway.getById).toHaveBeenCalledWith('1')
-    expect(response.statusCode).toEqual(200)
-    expect(response.product).toEqual(product)
+      expect(productGateway.getById).toHaveBeenCalledWith('1')
+      expect(response.statusCode).toEqual(200)
+      expect(response.product).toEqual(product)
+    })
   })
 })
--- src/interactors/ListProductsInteractor.test.js
+++ src/interactors/ListProductsInteractor.test.js
@@ -1,27 +1,29 @@
 const ListProductsInteractor = require('./ListProductsInteractor')
 
 describe('ListProductsInteractor', () => {
-  test('return an error response in gateway errors', async () => {
-    const listProductsInteractor = new ListProductsInteractor({
-      listAll: jest.fn().mockRejectedValue(new Error('Product gateway error.'))
+  describe('exec', () => {
+    test('return an error response in gateway errors', async () => {
+      const listProductsInteractor = new ListProductsInteractor({
+        listAll: jest.fn().mockRejectedValue(new Error('Product gateway error.'))
+      })
+      const response = await listProductsInteractor.exec()
+
+      expect(response.statusCode).toEqual(400)
+      expect(response.message).toEqual('Product gateway error.')
     })
-    const response = await listProductsInteractor.exec()
 
-    expect(response.statusCode).toEqual(400)
-    expect(response.message).toEqual('Product gateway error.')
-  })
+    test('return all products successfully', async () => {
+      const PRODUCT_LIST = [
+        { id: '1', name: 'Arroz Alteza 1Kg', price: 6.49 },
+        { id: '2', name: 'Feijão Kicaldo 1Kg', price: 7.99 }
+      ]
+      const listProductsInteractor = new ListProductsInteractor({
+        listAll: jest.fn().mockReturnValue(PRODUCT_LIST)
+      })
+      const response = await listProductsInteractor.exec()
 
-  test('return all products successfully', async () => {
-    const PRODUCT_LIST = [
-      { id: '1', name: 'Arroz Alteza 1Kg', price: 6.49 },
-      { id: '2', name: 'Feijão Kicaldo 1Kg', price: 7.99 }
-    ]
-    const listProductsInteractor = new ListProductsInteractor({
-      listAll: jest.fn().mockReturnValue(PRODUCT_LIST)
+      expect(response.statusCode).toEqual(200)
+      expect(response.products).toEqual(PRODUCT_LIST)
     })
-    const response = await listProductsInteractor.exec()
-
-    expect(response.statusCode).toEqual(200)
-    expect(response.products).toEqual(PRODUCT_LIST)
   })
 })
